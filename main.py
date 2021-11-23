3270a7d1cbb811393143239e97ba1e48a77e87fb
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2021-11-23 23:23:32 +0700

    gatau ini commit apa

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import abc
 import LexerFunc
-import cfg2cnf
 
-inputFile = input('Masukkkan nama file yang ingin di-lexing: ')
-tokenized= LexerFunc.startToken(inputFile)
-i=tokenized
-tes=[]
-i=i.split(' ')
-i = [string for string in i if string !='']
+inputFile = input("Masukkkan nama file yang ingin di-lexing: ")
+tokenized = LexerFunc.startToken(inputFile)
+i = tokenized
+tes = []
+i = i.split(" ")
+i = [string for string in i if string != ""]
 
-print(i)
+print(i.count("NEWLINE"))
+j = 1
+
+for x in i:
+    print(x, end=" ")
+    if x == "NEWLINE":
+        print(j)
+        j += 1
