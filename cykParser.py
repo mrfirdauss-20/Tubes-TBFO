d89e3db7a2db9cf475b0833711e50e7e051857af
fikrikhoironn <fikrikf470@gmail.com>
2021-11-24 21:42:09 +0700

    final touch in cykParser

--- cykParser.py
+++ cykParser.py
@@ -1,13 +1,6 @@
 from os import pipe
 import re
 
-# mapRegex = {
-#     r'[A-Za-z_][A-Za-z_0-9]*' : ["variable"],
-#     r'[0-9]*' : ["number"],
-#     r'[A-z0-9]*' : ["string"],
-# }
-# listRegex = [r'[0-9]*', r'[A-Za-z_][A-Za-z_0-9]*', r'[A-z0-9]*']
-
 global CNF
 CNF = {}
 
@@ -41,7 +34,7 @@ def getCNF(pathCNF):
             else:
                 CNF[rhs[item]].append(lhs)
     # print("Panjang Grammar: "+str(lengthGrammar))
-    # print(CNF)            
+    print(CNF)            
 
 
 def cykParser(input):
@@ -49,7 +42,6 @@ def cykParser(input):
     print(inputLength)
     # inisialisasi pada tabel CYK
     table = [[[] for j in range(i)] for i in range((inputLength), 0, -1)]
-    #print(table)
     # mengisi baris awal dengan mencari apakah ada production yang cocok dengan input
     for i in range(inputLength):
         # apabila ada aturan produksi yang cocok dengan input, masukkan aturan produksi ke tabel
@@ -59,16 +51,6 @@ def cykParser(input):
             continue
             # print("belum terdefinisi untuk "+input[i])
             
-        # jika tidak ada, coba cek apakah itu sebuah string/number/variabel dengan regex
-        # except KeyError:
-        #     for targetText in listRegex:
-        #         #jika ternyata string/number/variabel, masukkan aturan produksi tempat kemunculan hasil
-        #         if(re.match(targetText, input[i])):
-        #             for result in mapRegex[targetText]:
-        #                 try:
-        #                     table[0][i].extend(CNF[result])
-        #                 except KeyError:
-        #                     continue
     # mengisi tabel dengan algoritma CYK
     for i in range(1, inputLength):
         for j in range(inputLength - i):
@@ -82,11 +64,16 @@ def cykParser(input):
                             # print("tidak ditemukan untuk "+ result1+result2)
                             continue
 
-    for x in table:
-        print(x)
-    #print(table)
-    #print(table[-1][-1])
-    if (len(table[-1][-1]) != 0):
-        print("Accepted")
-    else:
-        print("wrong syntax")
+    # for x in table:
+    #     print(x)
+    
+    try:
+        if (len(table[-1][-1]) != 0 or len(table[-2][0]) != 0 or len(table[-3][0]) != 0):
+            print("Accepted")
+        else:
+            print("wrong syntax")
+    except IndexError:
+        if (len(table[-1][-1]) != 0):
+            print("Accepted")
+        else:
+            print("wrong syntax")
