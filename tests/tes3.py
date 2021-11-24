3ae876300b3b8e8df76df28daed1e688e76a40ae
mrfirdauss-20 <16520456@std.stei.itb.ac.id>
2021-11-24 22:33:26 +0700

    nambah tc

--- tests/tes3.py
+++ tests/tes3.py
@@ -1,8 +1,6 @@
 import sys
 import time as t
 
-import cykParser
-import Lexer
 
 if len(sys.argv) > 1:
     modelPath = str(sys.argv[1])
@@ -18,12 +16,6 @@ if inputFile:
     lxr = Lexer.Lexer()
     lxr.lex_file(inputFile)
     tokenValues = list(map(lambda t: t.value, lxr.tokens))
-
-    if cykParser.cykParser(tokenValues):
-        print("Accepted.")
-    else:
-        print("Syntax Error.")
-
     t2 = time.perf_counter()
     print(f"Done in {t2 - t1}s", end="\n\n")
 
@@ -33,29 +25,10 @@ class LexState(Enum):
     HEX = auto()
     BIN = auto()
     OCT = auto()
-    ALPHA = auto()
-    DIGIT = auto()
-    ALNUM = auto()
-    SYMBOL = auto()
-    BLANK = auto()
-    NUM = auto()
-    XBONUM = auto()
-    WORD = auto()
     STR = auto()
     STR3 = auto()
     SQUOTE = auto()
     SQUOTE1 = auto()
     SQUOTE2 = auto()
-    SQUOTE3 = auto()
-    SQUOTE32 = auto()
-    SQUOTE31 = auto()
-    DQUOTE = auto()
-    DQUOTE1 = auto()
-    DQUOTE2 = auto()
-    DQUOTE3 = auto()
-    DQUOTE32 = auto()
-    DQUOTE31 = auto()
-    COMMENT = auto()
-    BACKSLASH = auto()
-    ILLEGAL = auto()
+
 
